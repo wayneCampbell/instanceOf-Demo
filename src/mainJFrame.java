88a4f534abcc9fabdb673d475566323fdc2d7e93
userWayneCampbell <wcampbell1995@gmail.com>
2017-04-03 19:52:45 -0400

    Changed JPanel elements

--- src/mainJFrame.java
+++ src/mainJFrame.java
@@ -26,17 +26,118 @@ public class mainJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        rectangleButton = new javax.swing.JButton();
+        triangleButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        listTextArea = new javax.swing.JTextField();
+        heightTextTextField = new javax.swing.JTextField();
+        widthTextField = new javax.swing.JTextField();
+        lb_Area = new javax.swing.JLabel();
+        lb_AreaValue = new javax.swing.JLabel();
+        lb_Perimeter = new javax.swing.JLabel();
+        lb_PerimeterValue = new javax.swing.JLabel();
+        lb_Angle = new javax.swing.JLabel();
+        lb_Select = new javax.swing.JLabel();
+        lb_AngleValue = new javax.swing.JLabel();
+        itemTextField = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        rectangleButton.setText("Rectangle");
+
+        triangleButton.setText("Triangle");
+
+        jLabel1.setText("Height");
+
+        jLabel2.setText("Width");
+
+        heightTextTextField.setText("                     ");
+
+        widthTextField.setText("         ");
+
+        lb_Area.setText("Area:");
+
+        lb_AreaValue.setText("jLabel4");
+
+        lb_Perimeter.setText("Perimeter:");
+
+        lb_PerimeterValue.setText("jLabel6");
+
+        lb_Angle.setText("Angle:");
+
+        lb_Select.setText("Select Item #:");
+
+        lb_AngleValue.setText("jLabel9");
+
+        itemTextField.setText("    ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(triangleButton)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(rectangleButton)
+                                .addGap(61, 61, 61)
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(heightTextTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(28, 28, 28)
+                                .addComponent(jLabel2))
+                            .addComponent(listTextArea, javax.swing.GroupLayout.PREFERRED_SIZE, 299, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(lb_Select, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lb_Angle)
+                            .addComponent(lb_Perimeter)
+                            .addComponent(lb_Area)
+                            .addComponent(widthTextField))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(lb_AreaValue, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lb_PerimeterValue, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lb_AngleValue, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(itemTextField))))
+                .addContainerGap(35, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rectangleButton)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(heightTextTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(widthTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(triangleButton)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lb_Area)
+                            .addComponent(lb_AreaValue))
+                        .addGap(29, 29, 29)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lb_Perimeter)
+                            .addComponent(lb_PerimeterValue))
+                        .addGap(29, 29, 29)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lb_Angle)
+                            .addComponent(lb_AngleValue))
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lb_Select)
+                            .addComponent(itemTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(listTextArea, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,5 +179,20 @@ public class mainJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField heightTextTextField;
+    private javax.swing.JTextField itemTextField;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lb_Angle;
+    private javax.swing.JLabel lb_AngleValue;
+    private javax.swing.JLabel lb_Area;
+    private javax.swing.JLabel lb_AreaValue;
+    private javax.swing.JLabel lb_Perimeter;
+    private javax.swing.JLabel lb_PerimeterValue;
+    private javax.swing.JLabel lb_Select;
+    private javax.swing.JTextField listTextArea;
+    private javax.swing.JButton rectangleButton;
+    private javax.swing.JButton triangleButton;
+    private javax.swing.JTextField widthTextField;
     // End of variables declaration//GEN-END:variables
 }
